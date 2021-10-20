91e55f0b5cd287363a08c425b3e0014c7d05884b
Allan Pennycuff <allanp1238@gmail.com>
2021-10-20 10:36:22 -0500

    Completed the bonus section

--- script.js
+++ script.js
@@ -49,3 +49,40 @@ ghostbusters = () => "Who You Gonna Call?";
 const costumes = ["Ghost", "Princess", "Pirate"];
 // 11b
 const upperCostumes = costumes.map(i => i.toUpperCase());
+
+// BONUS
+// 12a
+const stephenKingMovies = [
+  "The Shining",
+  "Christine",
+  "It",
+  "The Mist",
+  "Creepshow",
+  "Pet Sematary"
+];
+// 12b
+const twoMovies = stephenKingMovies.filter(i => i.length === 9);
+console.log(twoMovies);
+// 13a
+const halloweenSongs1 = ["Monster Mash", "Thriller"];
+// 13b
+const halloweenSongs2 = [
+  "I Put A Spell On You",
+  "This Is Halloween",
+  "Ghostbusters"
+];
+// 13c
+halloweenJukebox = (songs) => {
+  const random = Math.floor(Math.random() * songs.length);
+  console.log(songs[random]);
+};
+// 13d
+halloweenJukebox(halloweenSongs1);
+halloweenJukebox(halloweenSongs2);
+// 14
+caps = (string) => {
+  for (i = 0; i < string.length; i += 2){
+    string = string.substring(0, i) + string[i].toUpperCase() + string.substring(i + 1);
+  }
+  return string;
+}
